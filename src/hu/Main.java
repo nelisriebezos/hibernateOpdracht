e9fcfbd056366e8a821d7d1df119069bda424a63
nelisriebezos <73901597+nelisriebezos@users.noreply.github.com>
2021-10-12 22:08:37 +0200

    Update Main.java

--- src/hu/Main.java
+++ src/hu/Main.java
@@ -33,6 +33,8 @@ public class Main {
         return factory.openSession();
     }
 
+//    testing
+    
     public static void main(String[] args) throws SQLException {
         testFetchAll();
     }
